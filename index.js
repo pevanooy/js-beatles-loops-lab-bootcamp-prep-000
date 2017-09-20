485b492a8b5270c6c3b60f99108c9485945ab9d6
Peter van Ooy <pevanooy@gmail.com>
2017-09-20 23:15:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay(musicians, instruments){
+  var myArray = [];
+  for (var i=0; i < musicians.length; i++){
+    myArray.push "John Lennon plays guitar";
+  }
+  return myArray;
+}
