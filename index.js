56fa01b58993ebec729a32c5182e7de11b673a59
Peter van Ooy <pevanooy@gmail.com>
2017-09-20 23:28:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function theBeatlesPlay(musicians, instruments){
 
 }
 function johnLennonFacts(facts){
-  var i;
+  var i = 0;
     while (i < facts.length) {
       facts[i] = facts[i] + "!!!"
       i++
