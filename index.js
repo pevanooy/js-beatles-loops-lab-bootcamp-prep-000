54814f5a3d91102c709ce99da0c085458a4236c0
Peter van Ooy <pevanooy@gmail.com>
2017-09-20 23:20:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments){
   var myArray = [];
   for (var i=0; i < musicians.length; i++){
-    myArray.push(``"$musicians[i] plays $instruments[0]"``);
+    myArray.push(`"$musicians[i] plays $instruments[0]"`);
   }
   return myArray;
 }
